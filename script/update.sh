066ed0d01334e4fbc0e323b7482c1f3a4254858a
leituo56 <leituo56@gmail.com>
2014-04-14 00:27:24 -0700

    update script
    update script

--- script/update.sh
+++ script/update.sh
@@ -1,11 +1,9 @@
 #!  /bin/bash
-sudo su - root
-service httpd stop
+sudo service httpd stop
 cd /home/ec2-user/repo/cmpe272/src/
-cp -r SocialNwk /var/www/
+sudo cp -r SocialNwk /var/www/
 cd /var/www/SocialNwk
-rm db.sqlite3
-python manage.py syncdb
-chown apache db.sqlite3
-service httpd start
-exit
+sudo rm db.sqlite3
+sudo python manage.py syncdb
+sudo chown apache db.sqlite3
+sudo service httpd start
