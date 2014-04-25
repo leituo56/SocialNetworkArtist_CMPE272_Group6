aea75dfbc3f945c0bd36da0fb6d45a5c0fba1e29
Tuo Lei <leituo56@gmail.com>
2014-04-25 03:47:27 -0700

    Init data script and photos
    Init data script and photos

--- script/update.sh
+++ script/update.sh
@@ -1,4 +1,6 @@
 #!  /bin/bash
+# Run source update.sh to execute
+
 sudo service httpd stop
 cd /home/ec2-user/repo/cmpe272/src/
 sudo cp -r SocialNwk /var/www/
@@ -11,4 +13,5 @@ cd /var/www/SocialNwk
 
 sudo python manage.py syncdb
 sudo chown apache db.sqlite3
+sudo python init_data.py
 sudo service httpd start
