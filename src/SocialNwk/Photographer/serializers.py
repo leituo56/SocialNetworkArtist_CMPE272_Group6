218071c8f0e47383785be55be4f08f3269415851
Xiumei Lu <sammiexiu@gmail.com>
2014-04-27 15:49:36 -0700

    Rollback

--- src/SocialNwk/Photographer/serializers.py
+++ src/SocialNwk/Photographer/serializers.py
@@ -9,20 +9,37 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name='photo:user-detail')
     follows = serializers.Field(source='profile.follows.all.values')
     followers = serializers.Field(source='profile.followers.all.values')
+    is_follow = serializers.SerializerMethodField('is_follow_already')
+    fav_make = serializers.Field(source='profile.fav_make')
+    fav_model = serializers.Field(source='profile.fav_model')
+    fav_category = serializers.Field(source='profile.fav_category')
+
+    def is_follow_already(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated():
+            lists = [item['user'] for item in user.profile.follows.values('user')]
+            return obj.id in lists
+        return False
 
     class Meta:
         model = User
-        fields = ('url', 'id', 'username', 'works', 'follows', 'followers')
-
+        fields = ('url', 'id', 'username', 'works', 'follows', 'followers', 'is_follow',
+                  'fav_make', 'fav_model', 'fav_category')
         #depth = 1
 
 
 class PhotoSerializer(serializers.HyperlinkedModelSerializer):
-    #author = serializers.Field(source='author.id')
+    #author = serializers.RelatedField()
     author = serializers.PrimaryKeyRelatedField(read_only=True)
     authorName = serializers.Field(source='author.username')
-    #author = serializers.RelatedField()
     url = serializers.HyperlinkedIdentityField(view_name='photo:photo-detail')
+
+    portrait = serializers.Field(source='portrait')
+    landscape = serializers.Field(source='landscape')
+    telephoto = serializers.Field(source='telephoto')
+    low_light = serializers.Field(source='low_light')
+    high_speed = serializers.Field(source='high_speed')
+    long_exposure = serializers.Field(source='long_exposure')
 
     def transform_file(self, obj, value):
         if obj is not None:
@@ -32,5 +49,7 @@ class PhotoSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Work
         fields = ('url', 'id', 'author', 'authorName',
-                  'title', 'file', 'upload_time')
+                  'title', 'file', 'upload_time', 'make', 'model', 'exposure_time',
+                  'fnumber', 'focal_length', 'iso', 'processing_software',
+                  'portrait', 'landscape', 'telephoto', 'low_light', 'high_speed', 'long_exposure')
         #depth = 1
--- src/SocialNwk/Photographer/tests.py
+++ src/SocialNwk/Photographer/tests.py
@@ -1,3 +1,67 @@
 from django.test import TestCase
+from Photographer.models import *
+import random
+
 
 # Create your tests here.
+class MyTest(TestCase):
+
+    def test_case_a(self):
+        tuolei, c = User.objects.get_or_create(username='tuolei')
+        tuolei.set_password('tuolei')
+        xiaolijiang, c = User.objects.get_or_create(username='xiaolijiang')
+        xiaolijiang.set_password('xiaolijiang')
+        wenjiazhang, c = User.objects.get_or_create(username='wenjiazhang')
+        wenjiazhang.set_password('wenjiazhang')
+        tuolei.save()
+        wenjiazhang.save()
+        xiaolijiang.save()
+
+
+        uname = [tuolei, xiaolijiang, wenjiazhang]
+        make = ['Canon', 'Nikon', 'Olympas']
+        model = ['400D', 'D50']
+        exposure_time = [0.0001, 10]
+        fnumber = [2, 10]
+        focal_length = [20, 150]
+        iso = [100, 8000]
+        for i in range(12):
+            p = []
+            temp = Work(author=random.choice(uname), title='My Pic'+str(i), file='init/' + str(i) + '.jpg',
+                        make=random.choice(make), model=random.choice(model),
+                        exposure_time=random.choice(exposure_time), fnumber=random.choice(fnumber),
+                        focal_length=random.choice(focal_length), iso=random.choice(iso),
+                        processing_software='Photoshop')
+            temp.save()
+            p.append(temp)
+        tuolei.profile.follows.add(xiaolijiang.profile)
+        xiaolijiang.profile.follows.add(wenjiazhang.profile)
+        wenjiazhang.profile.follows.add(tuolei.profile)
+
+    def test_case_b(self):
+        users = []
+        for i in range(100):
+            username = 'test_user' + str(i)
+            temp, c = User.objects.get_or_create(username=username)
+            temp.set_password(username)
+            temp.save()
+            users.append(temp)
+
+        uname = users
+        make = ['Canon', 'Nikon', 'Olympas']
+        model = ['400D', 'D50']
+        exposure_time = [0.0001, 10]
+        fnumber = [2, 10]
+        focal_length = [20, 150]
+        iso = [100, 8000]
+        for i in range(1000):
+            p = []
+            temp = Work(author=random.choice(uname), title='My Pic Title '+str(i), file='init/' + str(i % 12) + '.jpg',
+                        make=random.choice(make), model=random.choice(model),
+                        exposure_time=random.choice(exposure_time), fnumber=random.choice(fnumber),
+                        focal_length=random.choice(focal_length), iso=random.choice(iso),
+                        processing_software='Photoshop')
+            temp.save()
+            p.append(temp)
+        for i in range(99):
+            users[i].profile.follows.add(users[i+1].profile)
